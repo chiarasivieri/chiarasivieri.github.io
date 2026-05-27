89858c733e6f41914bbcd2a710bc73ffe0acd112
Chiara <sivierichiara02@gmail.com>
2026-05-27 15:59:31 +0200

    padding e routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,4 @@
-// routes.jsx - VERSIONE CORRETTA
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Home from './pages/Home';
@@ -8,7 +6,6 @@ import Contacts from './pages/Contacts';
 
 export default function AppRoutes() {
   return (
-    // Rimuovi completamente il prop 'basename'
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
